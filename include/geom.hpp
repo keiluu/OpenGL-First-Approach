9102ce6fd24c24496fa9e46d48c89d96a1b6f6b8
Lukas <100472939@alumnos.uc3m.es>
2025-12-24 00:23:29 +0100

    Vector and Matrix classes constructors and some operators

--- include/geom.hpp
+++ include/geom.hpp
@@ -1,23 +1,105 @@
-//
-// Created by batman on 12/20/25.
-//
-
 #ifndef GEOM_VISUALIZER_GEOM_HPP
 #define GEOM_VISUALIZER_GEOM_HPP
 
+#include <print>
+#include <experimental/mdspan>
+
 namespace  geom {
+    template<typename T>
+    class Mat3x3 {
+    private:
+        std::array<T, 9> array;
+        std::mdspan<T, std::extents<size_t, 3, 3>> matrix;
+
+    public:
+        Mat3x3() : array{}, matrix(array.data()) {}
+        explicit Mat3x3(std::array<T, 9> &other) : array(other), matrix(array.data()) {}
+
+        T& operator[](std::size_t i, std::size_t j) {
+            if (3*i + j >= 9) {
+                throw std::out_of_range("Mat3x3 index out of range");
+            }
+            return matrix[i, j];
+        }
+
+        const T& operator[](std::size_t i, std::size_t j) const {
+            if (3*i + j >= 9) {
+                throw std::out_of_range("Mat3x3 index out of range");
+            }
+            return matrix[i, j];
+        }
+
+        T& operator[](std::size_t i) {
+            if (i >= 9) {
+                throw std::out_of_range("Mat3x3 index out of range");
+            }
+            return array[i];
+        }
+
+        const T& operator[](std::size_t i) const {
+            if (i >= 9) {
+                throw std::out_of_range("Mat3x3 index out of range");
+            }
+            return array[i];
+        }
+
+    };
+
 
     template<typename T>
-    class Point3 {
+    class Vec3 {
     public:
         T x;
         T y;
         T z;
 
-        Point3() : x(0), y(0), z(0) {}
-        Point3(T x, T y, T z) : x(x), y(y), z(z) {}
-    };
+        constexpr Vec3() : x(0), y(0), z(0) {}
+        constexpr Vec3(T x, T y, T z) : x(x), y(y), z(z) {}
 
+        constexpr Vec3 operator+ (const Vec3& other) const {
+            return Vec3(x + other.x, y + other.y, z + other.z);
+        }
+
+        constexpr Vec3 operator- (const Vec3& other) const {
+            return Vec3(x - other.x, y - other.y, z - other.z);
+        }
+
+        constexpr Vec3 operator* (T scalar) const {
+            return Vec3(x * scalar, y * scalar, z * scalar);
+        }
+
+        constexpr Vec3 operator/ (T scalar) const {
+            return Vec3(x / scalar, y / scalar, z / scalar);
+        }
+
+        constexpr Vec3& operator+= (const Vec3& other) {
+            x += other.x;
+            y += other.y;
+            z += other.z;
+            return *this;
+        }
+
+        constexpr Vec3& operator-= (const Vec3& other) {
+            x -= other.x;
+            y -= other.y;
+            z -= other.z;
+            return *this;
+        }
+
+        constexpr Vec3& operator*= (T scalar) {
+            x *= scalar;
+            y *= scalar;
+            z *= scalar;
+            return *this;
+        }
+
+        constexpr Vec3& operator/= (T scalar) {
+            x /= scalar;
+            y /= scalar;
+            z /= scalar;
+            return *this;
+        }
+    };
 }; // namespace
 
 #endif //GEOM_VISUALIZER_GEOM_HPP
--- test/geom_tests.cpp
+++ test/geom_tests.cpp
@@ -3,13 +3,139 @@
 #include <print>
 #include <string>
 
-TEST(Geometry_Class, Point3_Constructor) {
+TEST(Geometry_Class, Vec3_Constructor) {
     try {
-        geom::Point3<double> pt(1.0, 2.0, 3.0);
-        std::println("Point3 coords -> x: {}   y: {}   z: {}", pt.x, pt.y, pt.z);
+        geom::Vec3<double> pt(1.0, 2.0, 3.0);
+        std::println("Construct Point3 coords -> x: {}   y: {}   z: {}", pt.x, pt.y, pt.z);
+
+        geom::Vec3<double> pt2;
+        pt2.x = 5.5;
+        std::println("Construct Point3 coords -> x: {}   y: {}   z: {}", pt2.x, pt2.y, pt2.z);
         SUCCEED();
 
-    } catch (const std::invalid_argument& e) {
+    } catch (const std::exception& e) {
+        std::println("Error: {}", e.what());
+        FAIL();
+    }
+    catch (...) {
+        FAIL();
+    }
+}
+
+TEST(Geometry_Class, Vec3_sum) {
+    try {
+        constexpr geom::Vec3<double> pt(1.0, 2.0, 3.0);
+        constexpr geom::Vec3<double> pt2(6.0, 7.0, 8.0);
+        constexpr geom::Vec3<double> pt3 = pt + pt2;
+
+        std::println("Sum Point3 coords -> x: {}   y: {}   z: {}", pt3.x, pt3.y, pt3.z);
+
+        SUCCEED();
+
+    } catch (const std::exception& e) {
+        std::println("Error: {}", e.what());
+        FAIL();
+    }
+    catch (...) {
+        FAIL();
+    }
+}
+
+TEST(Geometry_Class, Vec3_diff) {
+    try {
+        constexpr geom::Vec3<double> pt(1.0, 2.0, 3.0);
+        constexpr geom::Vec3<double> pt2(6.0, 7.0, 8.0);
+        constexpr geom::Vec3<double> pt3 = pt - pt2;
+
+        std::println("Diff Point3 coords -> x: {}   y: {}   z: {}", pt3.x, pt3.y, pt3.z);
+
+        SUCCEED();
+
+    } catch (const std::exception& e) {
+        std::println("Error: {}", e.what());
+        FAIL();
+    }
+    catch (...) {
+        FAIL();
+    }
+}
+
+TEST(Geometry_Class, Vec3_prod) {
+    try {
+        constexpr geom::Vec3<double> pt(1.0, 2.0, 3.0);
+        constexpr geom::Vec3<double> pt3 = pt * 4;
+
+        std::println("Prod Point3 coords -> x: {}   y: {}   z: {}", pt3.x, pt3.y, pt3.z);
+
+        SUCCEED();
+
+    } catch (const std::exception& e) {
+        std::println("Error: {}", e.what());
+        FAIL();
+    }
+    catch (...) {
+        FAIL();
+    }
+}
+
+TEST(Geometry_Class, Vec3_div) {
+    try {
+        constexpr geom::Vec3<double> pt(1.0, 2.0, 3.0);
+        constexpr geom::Vec3<double> pt3 = pt / 4;
+
+        std::println("Div Point3 coords -> x: {}   y: {}   z: {}", pt3.x, pt3.y, pt3.z);
+
+        SUCCEED();
+
+    } catch (const std::exception& e) {
+        std::println("Error: {}", e.what());
+        FAIL();
+    }
+    catch (...) {
+        FAIL();
+    }
+}
+
+TEST(Geometry_Class, Vec3_sumeq) {
+    try {
+        geom::Vec3<double> pt(1.0, 2.0, 3.0);
+        pt += geom::Vec3<double>(1.0, 2.0, 3.0);
+
+        std::println("SumEq Point3 coords -> x: {}   y: {}   z: {}", pt.x, pt.y, pt.z);
+
+        SUCCEED();
+
+    } catch (const std::exception& e) {
+        std::println("Error: {}", e.what());
+        FAIL();
+    }
+    catch (...) {
+        FAIL();
+    }
+}
+
+
+TEST(Matrix_Class, Mat3_Constructor) {
+    try {
+        std::array<int, 9> a {1,2,3};
+        geom::Mat3x3<int> m(a);
+        std::println("Without mdspan");
+        for (int i = 0; i < 9; i++) {
+            std::print("{} ",m[i]);
+        }
+        std::print("\n");
+
+        std::println("With mdspan");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                std::print("{} ",m[i, j]);
+            }
+            std::print("\n");
+        }
+
+        SUCCEED();
+
+    } catch (const std::exception& e) {
         std::println("Error: {}", e.what());
         FAIL();
     }
